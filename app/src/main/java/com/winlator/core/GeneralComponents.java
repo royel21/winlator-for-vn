f260c0fd54e1351d81778e4fc51f0a0f9bb3ab77
royel21 <royelconsoro@hotmail.com>
2026-07-31 09:21:19 -0400

    add some components

--- app/src/main/java/com/winlator/core/GeneralComponents.java
+++ app/src/main/java/com/winlator/core/GeneralComponents.java
@@ -125,16 +125,16 @@ public abstract class GeneralComponents {
 
         switch (type) {
             case BOX64:
-                items = new String[]{DefaultVersion.BOX64, "0.3.6", "0.3.8", "0.4.1"};
+                items = new String[]{DefaultVersion.BOX64, "0.3.6", "0.3.8", "0.4.1", "0.4.3"};
                 break;
             case TURNIP:
-                items = new String[]{DefaultVersion.TURNIP, "25.3.0", "26.1.0"};
+                items = new String[]{DefaultVersion.TURNIP, "25.3.0", "26.1.0", "26.1.15"};
                 break;
             case DXVK:
-                items = new String[]{DefaultVersion.MINOR_DXVK, DefaultVersion.MAJOR_DXVK, "2.7.1"};
+                items = new String[]{DefaultVersion.MINOR_DXVK, DefaultVersion.MAJOR_DXVK, "2.7.1", "3.0.2" };
                 break;
             case VKD3D:
-                items = new String[]{DefaultVersion.VKD3D};
+                items = new String[]{DefaultVersion.VKD3D, "3.0.1"};
                 break;
             case WINED3D:
                 items = new String[]{DefaultVersion.WINED3D};
